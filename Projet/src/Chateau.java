5f6419c59edd9cbe92091c9886faedb198ba476a
Remijav <57032950+Remijav@users.noreply.github.com>
2020-04-06 15:29:45 +0200

    Add files via upload

---
+++ Projet/src/Chateau.java
@@ -0,0 +1,7 @@
+package Troupes;
+
+public class Chateau extends Troupes{
+	public Chateau(Coordonnees coord){
+		super(50,0,coord,"Chateau");
+	}
+}
---
+++ Projet/src/Mage.java
@@ -0,0 +1,7 @@
+package Troupes;
+
+public class Mage extends Troupes{
+	public Mage(Coordonnees coord){
+		super(80,30,coord,"Mage");
+	}
+}
