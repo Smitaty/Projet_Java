92ae9f78fbf1092e72b74181b3c0ad997fc531a0
Smitaty <mitatysimon49@gmailcom>
2020-05-09 15:16:22 +0200

    Mis à jour des fichiers

--- Projet/src/Perceptron/IA.java
+++ Projet/src/Perceptron/IA.java
@@ -15,33 +15,42 @@ public class IA {
 		int rouge = 0;
 		int egalite = 0;
 		double somme = 0;
+		ArrayList<Jeu> list = new ArrayList<Jeu>();
+		ArrayList<Thread> thread = new ArrayList<Thread>();
 		for(int i=0; i<100; ++i) {
 			Plateau plateau = new Plateau(plat.getFile());
 			Strategie stratbleue = new StrategieRandom(plateau);
 			Strategie stratrouge = new StrategieRandom(plateau);
 			Jeu jeu = new Jeu(plateau, stratbleue, stratrouge, nbtour, true);
-			Thread t1 = new Thread(jeu);
+			list.add(jeu);
+		}
+		for(int i=0; i<100; ++i) {
+			Thread t1 = new Thread(list.get(i));
+			thread.add(t1);
 			t1.start();
+		}
+		for(int i=0; i<100; ++i) {
 			try{
-				t1.join();
+				thread.get(i).join();
 			}catch(InterruptedException e) {
 				e.printStackTrace();
 			}
-			if(jeu.isPartieFini()) {
-				if(jeu.isGagneBleu()) {
+			if(list.get(i).isPartieFini()) {
+				if(list.get(i).isGagneBleu()) {
 					++bleu;
 				}
 				else {
-					if(jeu.isGagneRouge()) {
+					if(list.get(i).isGagneRouge()) {
 						++rouge;
 					}
 					else 
 						++egalite;
 				}
 			}
-			somme+=jeu.getReward();
+			somme+=list.get(i).getReward();
 		}
 		double avg = somme/100;
+		System.out.println(somme);
 		System.out.println("Victoire bleue : "+bleu+" Victoire rouge : "+rouge+" Egalite : "+egalite);
 		System.out.println("Récompense moyenne : "+avg);
 	}
