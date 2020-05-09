b876ddd5c26c22944eeb7401924daf474aa93e26
Smitaty <mitatysimon49@gmailcom>
2020-05-09 15:15:29 +0200

    Mis à jour des fichiers

--- Projet/src/Game/Main.java
+++ Projet/src/Game/Main.java
@@ -16,22 +16,3 @@ public class Main {
 	//	perceptron.vizualise(100,plateau);
 	}
 }
-=======
-package Game;
-
-import IG.*;
-import Perceptron.*;
-import java.util.ArrayList;
-
-public class Main {
-
-	public static void main(String[] args) {
-		
-		Plateau plateau = new Plateau("src/Layout/Perceptron.lay");
-		IA perceptron = new IA();
-		ArrayList<Jeu> list = perceptron.rechercheAleatoire(100, plateau);
-		double meilleurScore = perceptron.getMeilleurScore(list);
-		System.out.println("Meilleur score final= " + meilleurScore);
-	//	perceptron.vizualise(100,plateau);
-	}
-}
